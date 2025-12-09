76b14a9998313df34cd474cbb8f9f4fa5fdd2371
eesa <eesayousufyousuf@gmail.com>
2025-12-09 12:01:00 +0530

    stringmethod

---
+++ chapter2/chapter3/stringMethod.py
@@ -0,0 +1,13 @@
+str= "Eesa yousuf"
+print(str.upper())
+print(str.lower())
+print(str.title())
+print(str.find("ng"))
+print(str.replace("english", "Best"))
+print(str.count("a")) #count the occurence 
+
+#escape sequence 
+
+print("Hello World")
+print("Hello\nWorld")
+print("Hello \t World")
